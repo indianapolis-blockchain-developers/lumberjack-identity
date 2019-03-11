bcf85548f75dd2e6e47b9b1afebd733e463dce10
Gabriel Rodriguez <gabrielrodriguez@northwesternmutual.com>
2019-03-11 01:16:10 -0400

    March 10 Updates * Provisioned the Bastion host to install ansible locally. The bastion host will provision the Kubernetes Nodes. * Staged the ec2.py dynamic script and ec2.ini configuration file. This will allow an Ansible server to generate dynamic inventory from AWS. * Added an Ansible Directory and directory structure template where we will store our playbooks

--- devops/terraform/live/stage/ami.tf
+++ devops/terraform/live/stage/ami.tf
@@ -12,6 +12,28 @@ resource "aws_instance" "bastion" {
     tags {
         Name = "bastion"
     }
+
+    provisioner "remote-exec" {
+        inline = [
+            "sudo yum -y update",
+            "sudo yum -y install epel-release",
+            "sudo yum -y install ansible",
+            "sudo yum -y install python-boto",
+            "sudo yum -y install vim",
+            "mkdir inventory",
+            "curl -O https://raw.githubusercontent.com/ansible/ansible/devel/contrib/inventory/ec2.py",
+            "curl -O https://raw.githubusercontent.com/ansible/ansible/devel/contrib/inventory/ec2.ini",
+            "mv ec2.* ~/inventory/",
+            "chmod +x ~/inventory/ec2.py "
+        ]     
+    }
+
+        connection {
+            type = "ssh"
+            user = "centos"
+            private_key = "${file("~/.ssh/bastion_key.pem")}"
+        }
+
 }
 
      
@@ -22,7 +44,7 @@ resource "aws_instance" "rke-node" {
   ami                    = "${lookup(var.amis, var.aws_region)}"
   instance_type          = "${var.InstanceType}"
   key_name               = "${aws_key_pair.rke-node-key.id}"
-  # iam_instance_profile   = "${aws_iam_instance_profile.rke-aws.name}"
+  iam_instance_profile   = "${aws_iam_instance_profile.rke-aws.name}"
   vpc_security_group_ids = ["${aws_security_group.private_sg.id}"]
   subnet_id              = "${aws_subnet.private.id}"
   root_block_device {
@@ -30,3 +52,4 @@ resource "aws_instance" "rke-node" {
   }  
   tags {Name = "rke"}
 }
+
